9a42acc21b462a15568784cfb04979ef84195fc9
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-07-12 17:19:01 -0500

    looked at different types of operators

---
+++ operators.py
@@ -0,0 +1,77 @@
+#opertators are used to execute an operation between two operands
+
+##types of opertators
+
+#assignment opertators ex. =
+totalChickens = 10
+print(totalChickens)
+#prints out 10
+
+
+#arithmetic Operators ex. + - * / %
+print(1+1)
+#prints 2
+print(1-1)
+#prints 0
+print(2*2)
+#prints 4
+print(2/2)
+#prints 1.0
+#in this case the int becomes a float, to prevent that from happening you need to use two /
+print(15%10)
+#prints 5
+#in this case 5 is the remainder of the division of 15/10
+
+
+#relational Operators ex. <, >, ==, <=, >= and !=
+print(2<5)
+#prints true
+
+print(5<5)
+#prints False
+
+print(5<=5)
+#prints true
+
+print(6<=5)
+#prints false
+
+print(5>2)
+#prints true
+
+print(2>5)
+#prints false
+
+print(5>=5)
+#prints true
+
+print(5>=6)
+#prints false
+
+print(5==5)
+#prints true
+
+print(5==2)
+#prints false
+
+print(5!=2)
+#prints true
+
+
+print(5!=5)
+#prints false
+
+
+#logical operator and, or and not
+
+print(2<5 and 5<10)
+#prints true
+
+print(2<5 and 5>10)
+#prints False
+
+print(2<5 or 5<10)
+#prints true
+
+print(5>10 or 2<5)
+#prints true
