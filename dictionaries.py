aa50d1aa208d28e862226d57e24cd66af1b1f175
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-07-13 13:08:07 -0500

    dictionary operations

--- dictionaries.py
+++ dictionaries.py
@@ -11,3 +11,37 @@ print(steakPrices["t-bone"])
 steakPrices["sirloin"] = 20.99
 print(steakPrices["sirloin"])
 #prints 20.99
+
+##Operations
+
+#dictionaryName.keys() calls out all the keys in that particular dictionary
+print(steakPrices.keys())
+#prints dict_keys(['sirloin', 't-bone', 'ribeye'])
+
+#dictionaryName.values() calls out all the values in that particular dictionary
+print(steakPrices.values())
+#prints dict_values([20.99, 27.99, 29.99])
+
+#to clone a dictionary you can use dictionaryName.copy()
+#Ex.
+copyOfSteakPrices = steakPrices.copy()
+print(copyOfSteakPrices)
+#prints {'sirloin': 20.99, 't-bone': 27.99, 'ribeye': 29.99}
+
+#to delete a key-value pair in a dictionary..
+del steakPrices["t-bone"]
+print(steakPrices)
+#prints {'sirloin': 20.99, 'ribeye': 29.99}
+
+#to clear the contents of a dictionary..
+steakPrices.clear()
+print(steakPrices)
+#prints {}
+
+# to add a key-value pair to a dictionary..
+steakPrices["t-bone"] = 27.99
+print(steakPrices)
+#prints {'t-bone': 27.99}
+steakPrices["ribeye"] = 29.99
+print(steakPrices)
+#prints {'t-bone': 27.99, 'ribeye': 29.99}
