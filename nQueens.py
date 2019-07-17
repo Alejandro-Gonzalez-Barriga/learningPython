398040f38efdea71ea881f4c87637d8d9dcb0b0c
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-07-16 20:09:32 -0500

    promted user to input the size of the chez board

---
+++ nQueens.py
@@ -0,0 +1,10 @@
+#first we will print the matrix
+
+print("Enter num: ")
+
+num = int(input())
+
+for row in range(1, 8):#iterates through rows in matrix
+  for column in range(1, 8):#iterates through the columns the matrix
+    print(0, end = " ")#prints the num and ends with a space after every print statement
+  print()#this empty print function will print the num dymanically after every column loop is executed
