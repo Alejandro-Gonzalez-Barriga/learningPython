ebf08ba4969dbbccfd0cd6bf0c1455c4d224d283
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-07-16 20:10:42 -0500

    corrected the max range parameter

--- nQueens.py
+++ nQueens.py
@@ -4,7 +4,7 @@ print("Enter num: ")
 
 num = int(input())
 
-for row in range(1, 8):#iterates through rows in matrix
-  for column in range(1, 8):#iterates through the columns the matrix
+for row in range(1, num):#iterates through rows in matrix
+  for column in range(1, num):#iterates through the columns the matrix
     print(0, end = " ")#prints the num and ends with a space after every print statement
   print()#this empty print function will print the num dymanically after every column loop is executed
