70dd2288ffe18678de07479acf264c88889908da
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-07-19 05:15:07 -0500

    iniciated a class with a custom constructor and placeholders

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ language.
 Sources:
 https://www.youtube.com/watch?v=wfcWRAxRVBA
 https://www.udemy.com/python-programming-beginners/learn/lecture/6693338#overview
+https://www.youtube.com/watch?v=Z1Yd7upQsXY&list=PLBZBJbE_rGRWeh5mIBhD-hhDwSEDxogDg
--- nQueens_Python3/nQueens.py
+++ nQueens_Python3/nQueens.py
@@ -1,4 +1,12 @@
-#first we will print the matrix
+class NQueens:#we define a class to build different matrix representation fo each of the possible solution
+
+    def __init__(self, size):#the constructor takes the parameter of size to calculate the different solutions depending on the size of the board the user decides to input
+        self.size = size#the size of the board
+        self.solutions = 0#the counter for each solution found
+        self.solve()#this method solves for solutions depending on the size of the board
+        
+
+
 
 print("Enter num: ")
 
